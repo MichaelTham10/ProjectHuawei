bca9006b8bcbe8b898684de8ac73b53788831924
MichaelTham10 <mikeltham01@gmail.com>
2021-12-18 13:41:40 +0700

    first commit

---
+++ app/src/main/java/com/example/projecthuawei/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.projecthuawei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huawei.hmf.tasks.Task;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+}
